fbf0f5ab69b9c8b9c82ce4e2b906ebdb41622c24
KingParry <kingparry@yahoo.com>
2023-11-28 19:23:07 -0800

    Update Python0X00

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,12 +1,15 @@
 #include "lists.h"
+
 /**
  * check_cycle - beginning of cycle checks
  * @list: lnked list of cycle 
  * Return: 1, 0 if list has cycle and not respectively
  */
+ 
 int check_cycle(listint_t *list)
 {
 	listint_t *s = list, *f = list;
+
 	if (!list)
 		return (0);
 	while (s && f && f->next)
@@ -17,3 +20,4 @@ int check_cycle(listint_t *list)
 			return (1);
 	}
 	return (0);
+}
