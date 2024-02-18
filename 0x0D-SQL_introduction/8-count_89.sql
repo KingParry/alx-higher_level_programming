c8894551ba928832b22a6bb0d7296558e28451d0
KingParry <kingparry@yahoo.com>
2024-02-18 03:42:25 -0800

    SQL Update

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 --displays the number of records with id = 89 in the table first_table
-Select Count(*) from 'first_table' where 'id' = 89;
+Select Count(*) from first_table where id = 89;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 --creates a table second_table in the database hbtn_0c_0 in your MySQL server
-create table if not exists 'second_table' ('id' int, 'name' varchar(256),'score, int);
-insert into 'second_table' ('id', 'name', 'score') values (1, "John",10);
-insert into 'second_table' ('id', 'name', 'score') values (2, "Alex",3);
-insert into 'second_table' ('id', 'name', 'score') values (3, "Bob",14);
-insert into 'second_table' ('id', 'name', 'score') values (4, "George",8);
+create table if not exists second_table (id int, name varchar(256),score, int);
+insert into second_table (id, name, score) values ('1', 'John','10');
+insert into second_table (id, name, score) values (2, 'Alex','3');
+insert into second_table (id, name, score) values ('3', 'Bob','14');
+insert into second_table (id, name, score) values ('4', 'George','8');
