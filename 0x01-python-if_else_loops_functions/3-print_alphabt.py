a7b70ea85b7980808fb92dd57bcc64bb32fd4840
KingParry <kingparry@yahoo.com>
2024-03-12 16:51:16 -0700

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i != 101 and i != 113:
-        print("{:c}".format(i), end='')
+# authr - Emml Parry
+for letty in range(97, 123):
+    if chr(letty) != 'e' and chr(letty) != 'q':
+        print("{}".format(chr(letty)), end="")
