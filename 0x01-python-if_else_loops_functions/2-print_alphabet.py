1e4dba10c7f1605feb26675a216777c460d68029
KingParry <kingparry@yahoo.com>
2024-03-12 16:36:51 -0700

    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print("{:c}".format(i), end='')
+import random
+namb = random.randint(-10000, 10000)
+digit = abs(namb) % 10
+if namb < 0:
+    digit = -digit
+print(f"Last digit of {namb:d} is {digit:d} and is ", end="")
+if digit > 5:
+    print("greater than 5")
+elif digit == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
