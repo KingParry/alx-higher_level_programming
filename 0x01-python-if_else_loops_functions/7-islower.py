34116cbf1666fc0142d6dfb35d9b7584f310eb34
KingParry <kingparry@yahoo.com>
2024-03-12 17:06:08 -0700

    update

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Creator - EP
+
+def islower(g):
+    """ a function that checks for lowercase character."""
+    if ord(g) >= 97 and ord(g) <= 122:
+        return True
+    else:
+        return False
